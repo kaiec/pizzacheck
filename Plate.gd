0e7bb618dd796dbd26153a6cad536d008864f829
Kai Eckert <hallo@kaiec.de>
2021-03-18 00:13:19 +0100

    Shuffling plate

---
+++ Plate.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var values = ["oregano", "peperoni", "mozarella", "corn", "tomato", "olives", "broccoli", "champignon"]
+var missing = "oregano"
+	
+	
+func shuffle():
+	var posnos = range(7)
+	posnos.shuffle()
+	values.shuffle()
+	missing = values[0]
+	for v in $vegetables.get_children():
+		if v.name == missing:
+			v.hide()
+			continue
+		v.position = $positions.get_child(posnos.pop_back()).position
+		var scale = rand_range(0.25, 0.5)
+		v.scale = Vector2(scale, scale)
+		v.show()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	shuffle()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
